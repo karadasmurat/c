ae2b161c9e3a5e948ab3c1cc18a085f9a2f9919e
karadasmurat <karadasmurat@gmail.com>
2022-10-06 01:53:27 +0300

    sizeof

--- pointers.c
+++ pointers.c
@@ -17,6 +17,10 @@ int main(void){
     // the value of *p
     printf("*p: %d\n", *p);
 
+    // size of a pointer to an integer
+    printf("int   :Storage size: %zu bytes\n", sizeof(int)); // 4 bytes (32bits)
+    printf("int * :Storage size: %zu bytes\n", sizeof(int *)); // 8 bytes (64bits)
+
 
     return 0;
 }
