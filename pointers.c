3a37a2b5be3d9a9964aee3f84cd03bf2cabb9082
karadasmurat <karadasmurat@gmail.com>
2022-10-08 12:35:18 +0300

    arrays

--- pointers.c
+++ pointers.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+void increment_by_val(int arg);
+void increment_by_ref(int *arg);
+void swap (int *a, int* b);
+
+
 int main(void){
 
     int n = 10, y=5;
@@ -8,7 +13,7 @@ int main(void){
     printf("n: %d, y: %d\n", n, y);
 
     int *p; // p is a pointer to int
-    p = &n; // p now points to n
+    p = &n; // p now points to n (so *p = 10 now)
 
     // the address of n
     printf("&n: %p\n", &n); // i.e. 0x16d28b2a0
@@ -19,7 +24,7 @@ int main(void){
     // the value of *p
     printf("*p: %d\n", *p); // 10
 
-    y = *p;	// y is now 10
+    y = *p + 4;	// y is now 14
     *p = 1;	// n is now 1 - modified through pointer!
 
 
@@ -30,6 +35,11 @@ int main(void){
     // size of a pointer to an integer (64 bits / 8 bytes on my machine)
     printf("int   :Storage size: %zu bytes\n", sizeof(int)); // 4 bytes (32bits)
     printf("int * :Storage size: %zu bytes\n", sizeof(int *)); // 8 bytes (64bits)
+
+    // if p points to integer, then *p can occur in any context where n could, so
+    *p += 4;
+    printf("*p: %d, n: %d\n", *p, n); // *p: 5, n: 5
+
 
     int scores[] = {95, 90,100};
     printf("\n\nscores    : %p\n", scores); // 0x16f53b2a8
@@ -44,5 +54,40 @@ int main(void){
     printf("&s[0]: %p\n", &s[0]); // the address of the first element
 
 
+    int count = 1;
+    printf("\n\ninitial value before function calls: %d\n", count);
+
+    increment_by_val(count);
+    printf("after function call by val: %d\n", count);
+
+    increment_by_ref(&count);
+    printf("after function call by ref: %d\n", count);
+
     return 0;
+}
+
+void increment_by_val(int arg){
+    arg += 1;
+}
+
+/* 
+* parameters are declared as pointers
+* the operands are accessed indirectly through them 
+* (caller pass the addresses of its variables, and called can modify them directly through these addresses)
+*/
+void increment_by_ref(int *arg){
+    *arg += 1;
+}
+
+/* 
+* parameters are declared as pointers
+* the operands are accessed indirectly through them 
+* (caller pass the addresses of its variables, and called can modify them directly through these addresses)
+*/
+void swap (int *a, int* b){
+
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+
 }
--- variables.c
+++ variables.c
@@ -1,10 +1,109 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+
+void enum_basics();
+void array_basics();
+void no_initialization_garbage();
 
 int main(void){
 
-    // limits
-    printf("INT_MAX: %d", INT_MAX);
-    
+    // limits defined in limits.h
+    printf("INT_MAX: %d\n", INT_MAX);
+    //unsigned int data type
+    printf("unsigned int\nStorage size: %zu bytes \t Maximum value: %u\n\n", sizeof(unsigned int), UINT_MAX);
+
+    /* Long decimal constants */
+    long long_val = 10L;
+
+    // the value of an integer can be specified in octal or hexadecimal instead of decimal
+    // leading zero means octal, leading 0x hexadecimal.
+    int decimal_val = 31;
+    int octal_val = 037;
+    int hex_val = 0X1F;
+
+    printf("Decimal: %d, Octal: %d, Hex: %d\n", decimal_val, octal_val, hex_val);
+
+
+    // increment
+    // postfix increments the variable AFTER its value is used.
+    int x = 10;
+    int y = x++;
+    printf("x: %d, y: %d\n", x, y); // x: 11, y: 10
+
+    // prefix increments the variable BEFORE its value is used.
+    x = 10;
+    y = ++x;
+    printf("x: %d, y: %d\n", x, y); // x: 11, y: 11
+
+    enum_basics();
+
+    array_basics();
+
+    no_initialization_garbage();
+
     return 0;
 }
+
+void enum_basics(){
+    // enumerations provide a convenient way to associate constant values with names.
+    // the first name in an enum has value 0, the next 1, and so on, unless explicit values are specified.
+    enum direction {North, South, East, West};
+    enum month{JAN=1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC}; //FEB is 2, MAR is 3, etc.
+
+    // declare a variable of type enum choice
+    enum direction nav;
+    enum month m;
+
+    // assignment
+    nav = West;
+    m = SEP;
+
+    printf("enum direction: %d\n", nav);
+    printf("enum month: %d\n", m);
+}
+void no_initialization_garbage(){
+    
+    int val[5];
+
+    for(int i = 0; i < 5; i++){
+        printf("val[%d]: %d\n", i, val[i]);
+    }
+}
+
+void array_basics(){
+
+    // option 1
+    // first declare the array with type, name and size, then initialize contents using index values
+    int grades[3];
+    
+    printf("The address of first element: %p\n", grades); // i.e. 0x16bb1324c
+
+    grades[0] = 90;
+    grades[1] = 80;
+    grades[2] = 95;
+
+
+    // option 2
+    // declare and initialize an array, size is inferred.
+    int val[] = {1, 2, 3, 5, 8};
+
+
+    // option 3 - dynamic memory allocation
+    // point the address of first element
+    int *years = malloc(3 * sizeof(int));
+    *years = 2019;
+    *(years + 1) = 2020;    // pointer arithmetic to get the next element
+    years[2] = 2022;        // index, to get the specific element
+
+    printf("The address of first element: %p\n", years);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("years[%d]: %d\n", i, years[i]);
+    }
+
+    // free memory
+    free(years);
+
+}
+
