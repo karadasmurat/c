b77889f4c17b77ff326762f8d8a87b600145cdc5
karadasmurat <karadasmurat@gmail.com>
2022-10-19 23:41:42 +0300

    selection sort

--- sort/selection_bubble_sort.c
+++ sort/selection_bubble_sort.c
@@ -5,6 +5,7 @@
 
 /* Function Prototypes*/
 void bubblesort(int argc, int* args);
+void selectionsort(int argc, int* args);
 
 int main(void){
     
@@ -15,6 +16,7 @@ int main(void){
     print_array(SIZE, scores);
 
     bubblesort(SIZE, scores);
+    // selectionsort(SIZE, scores);
 
     puts("Sorted array: ");
     print_array(SIZE, scores);
@@ -24,6 +26,11 @@ int main(void){
     return 0;
 }
 
+// Compare the current element with the one after it, swap their values if needed. 
+// The algorithm, which is a comparison sort, is named for the way the larger elements "bubble" up to the top of the list.
+
+// This simple algorithm performs poorly in real world 
+// O(n^2) worst case.
 void bubblesort(int argc, int* args){
 
     // as each index will be compared to the next index, n-1 total passes.
@@ -38,6 +45,29 @@ void bubblesort(int argc, int* args){
             }
 
         }
+
+        // result of this pass
+        // print_array(SIZE, args);
+    }
+
+}
+
+void selectionsort(int argc, int* args){
+
+    // For each pass, select the smallest
+    for(int pass = 0; pass < argc; pass++){
+
+        int* min = &args[pass];
+        for(int i = pass; i< argc-1; i++){
+
+            if(args[i+1] < *min) 
+                min = &args[i+1];
+        }
+
+        swap(min, &args[pass]);
+
+        // result of this pass
+        // print_array(SIZE, args);
     }
 
 }
